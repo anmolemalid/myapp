d09ba9dae964e5398843cb62fbb6500290c34355
anmol_guptasapient <anmol.gupta@publicissapient.com>
2020-05-25 10:43:55 +0530

    ui

---
+++ public/index.js
@@ -0,0 +1,38 @@
+(function (){
+
+
+  var request = new XMLHttpRequest();
+
+  // Open a new connection, using the GET request on the URL endpoint
+  request.open('GET', '/listUsers', true);
+  request.setRequestHeader('content-type', 'application/json');
+
+  request.onload = function() {
+    const userlist =  document.querySelector('.userlist');
+    let data = this.response;
+    data = JSON.parse(data);
+    console.log(data, 'dsdsdsfs');
+
+    data.forEach(user => {
+      const li = document.createElement('li');
+      const liContainer = document.createElement('div');
+      const name = document.createElement('label');
+      const password = document.createElement('label');
+      const profession = document.createElement('label');
+      name.innerText = user.name+', ';
+      password.innerText = user.password+', ';
+      profession.innerText = user.profession;
+      liContainer.appendChild(name);
+      liContainer.appendChild(password);
+      liContainer.appendChild(profession);
+      li.appendChild(liContainer);
+      userlist.appendChild(li);
+    });
+    // Begin accessing JSON data here
+  }
+  // Send request
+  request.send()
+
+
+
+})();
---
+++ server.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var app = express();
+var fs = require("fs");
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+const users = [
+   {
+     "name" : "mahesh",
+     "password" : "password1",
+     "profession" : "teacher",
+     "id": 1
+  },
+  {
+     "name" : "suresh",
+     "password" : "password2",
+     "profession" : "librarian",
+     "id": 2
+  },
+   {
+     "name" : "ramesh",
+     "password" : "password3",
+     "profession" : "clerk",
+     "id": 3
+  }
+];
+
+app.get('/listUsers', function (req, res) {
+   res.json( users );
+})
+
+var server = app.listen(8081, function () {
+   var host = server.address().address
+   var port = server.address().port
+   console.log("Example app listening at http://%s:%s", host, port)
+})
